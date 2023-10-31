947e882da06e39b843159255b2ee6ffe3b2741bf
haulam <hau.lam@intelematics.com>
2023-10-31 20:38:31 +0700

    add skeleton

---
+++ frontend/src/FlowerApp.js
@@ -0,0 +1,10 @@
+import Login from './components/Login/Login';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function FlowerApp() {
+  return (
+    <Login />
+  );
+}
+
+export default FlowerApp;
