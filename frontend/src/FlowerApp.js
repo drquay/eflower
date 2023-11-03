b0b10e8a9c566d6067d566c5780d6870b4bb13bf
haulam <hau.lam@intelematics.com>
2023-11-03 22:12:16 +0700

    add left menu

--- frontend/src/FlowerApp.js
+++ frontend/src/FlowerApp.js
@@ -1,9 +1,13 @@
-import Login from './components/Login/Login';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import Login from "./components/Login/Login";
+import LeftMenu from "./components/LeftMenu/LeftMenu";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function FlowerApp() {
   return (
-    <Login />
+    <>
+      {/* <Login /> */}
+      <LeftMenu />
+    </>
   );
 }
 
